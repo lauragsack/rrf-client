71ed0f8d4788f3daa56cd5c6f1a3547f04af65c2
lsack <lauragsack@gmail.com>
2020-05-04 21:22:10 -0700

    adds state & methods to App component

--- src/App.js
+++ src/App.js
@@ -3,16 +3,63 @@ import { withRouter } from 'react-router-dom';
 import Routes from './config/routes';
 import Navbar from './components/Navbar';
 import UserModel from './models/user';
+import FloatieModel from './models/floatie';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 class App extends Component {
+    state = {
+        currentUser: JSON.parse(localStorage.getItem("user")),
+        floatieList: [],
+    }
+
+    setCurrentUser = (user) => {
+        this.setState({currentUser: user})
+        localStorage.setItem("user", JSON.stringify(user))
+        localStorage.setItem("uid", user.id)
+        localStorage.setItem("uname", user.name)
+    }
+
+    logout = (event) => {
+        event.preventDefault();
+
+        localStorage.removeItem("user")
+        UserModel.logout()
+            .then(res => {
+                this.setState({currentUser: null})
+                this.props.history("/")
+            })
+            .catch(err => console.log(err))
+    }
+
+    async fetchFloatieList() {
+        let res = await.FloatieModel.all();
+        this.setState({
+            floatieList: res.data
+        })
+    }
+
+    componentDidMount() {
+        this.fetchFloatieList();
+    }
+
     render() {
         return (
             <>
                 <Navbar
-                    
+                    currentUser={this.state.currentUser}
+                    logout={this.logout}
+                    history={this.props.history}
+                    setCurrentUser={this.setCurrentUser}
+                    floatieList={this.state.floatieList}
                 />
+                <>
+                    <Routes
+                    currentUser={this.state.currentUser}
+                    setCurrentUser={this.setCurrentUser}
+                    floatieList={this.state.floatieList}
+                    />
+                </>
             </>
         );
     }
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,10 +1,92 @@
 import React, { Component } from 'react';
+import UserModel from '../models/user';
 
 class Signup extends Component {
+    state = {
+        name: "",
+        email: "",
+        password: "",
+        password2: "",
+        favFloatie: {},
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault()
+        UserModel.create(this.state)
+            .then(res => {
+                this.setState({
+                    name: "",
+                    email: "",
+                    password: "",
+                    password2: ""
+                })
+                this.props.setCurrentUser(res.data.data)
+                this.props.history,push("/account")
+            })
+            .catch(err => console.log(err))
+    }
+
     render() {
         return (
-            <div>
-                
+            <div className="container mt-4">
+                <div className="row">
+                    <div className="cold-md-4 offset-md-3">
+                        <h4 className="mb-3">Sign up</h4>
+                        <form onSubmit={this.handleSubmit}>
+                            <div className="form-group">
+                                <label htmlFor="name">Name</label>
+                                <input
+                                    onChange={this.handleChange}
+                                    className="form control form-control-lg"
+                                    type="text"
+                                    id="name"
+                                    name="name"
+                                    value={this.state.name}
+                                />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="email">Email</label>
+                                <input
+                                    onChange={this.handleChange}
+                                    className="form control form-control-lg"
+                                    type="email"
+                                    id="email"
+                                    name="email"
+                                    value={this.state.email}
+                                />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="password">Password</label>
+                                <input
+                                    onChange={this.handleChange}
+                                    className="form control form-control-lg"
+                                    type="password"
+                                    id="password"
+                                    name="password"
+                                    value={this.state.password}
+                                />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="password2">Confirm Password</label>
+                                <input
+                                    onChange={this.handleChange}
+                                    className="form control form-control-lg"
+                                    type="password"
+                                    id="password2"
+                                    name="password2"
+                                    value={this.state.password2}
+                                />
+                            </div>
+                            <button className="btn btn-primary pull-left" type="submit" onClick={this.props.onClick}>Sign up</button>
+                        </form>
+                    </div>
+                </div>
             </div>
         );
     }

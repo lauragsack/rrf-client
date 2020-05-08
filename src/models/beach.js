fa971e4d0ea789a25b9c8bdcc690783594ca4503
lsack <lauragsack@gmail.com>
2020-05-08 12:38:31 -0700

    updates REACT_APP_API_URL on models

--- src/models/beach.js
+++ src/models/beach.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const REACT_APP_API_URL = "http://localhost:3001/api/v1/beaches";
+const REACT_APP_API_URL = "mongodb://heroku_cf9hmk9p:n56sne2us9p3cbvchvuiqjece8@ds233531.mlab.com:33531/heroku_cf9hmk9p/api/v1/beaches";
 
 export default class BeachModel {
     static all = () => {

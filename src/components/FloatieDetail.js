7bdec202459c94ac2d945e951074e9eefc02c152
lsack <lauragsack@gmail.com>
2020-05-10 19:32:38 -0700

    commits floatie detail page style

--- src/components/FloatieDetail.js
+++ src/components/FloatieDetail.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Card from 'react-bootstrap/Card';
+import { Card, Container } from 'react-bootstrap';
 import FloatieModel from '../models/floatie';
 
 class FloatieDetail extends Component {
@@ -37,46 +37,45 @@ class FloatieDetail extends Component {
 
   render() {
     return (
-      <Card className="fl-detail-card">
-        <Card.Img className="fl-detail-img" variant="top" src={this.state.floatie.photo} />
-        <Card.Body>
-          <Card.Title className="fl-detail-title">{this.state.floatieName}</Card.Title>
-          <Card.Text>
-            {this.state.floatie.description}
-          </Card.Text>
-          <Card.Text>
-            {`Type: ${this.state.floatie.type}`}
-          </Card.Text>
-          <Card.Text>
-          {this.state.floatie.deliverable === true ?
-            <>
-              Can be delivered: <i className="fas fa-check"></i>
-            </>
-            :
-            <>
-              Can be delivered: <i className="fas fa-times"></i> 
-            </>
-          }
-          </Card.Text>     
-          <Card.Text>
-            {`Good for: ${this.state.floatie.goodFor}`}
-          </Card.Text>
-          <Card.Text>
-          {this.state.floatie.dogFriendly === true ?
-            <>
-              Dog Friendly: <i className="fas fa-paw"></i>
-            </>
-            :
-            <>
-              Dog Friendly: <i className="fas fa-times"></i> 
-            </>
-          }
-          </Card.Text>  
-          <Card.Text>
-            {`Price per day: $${this.state.floatie.price}`}
-          </Card.Text>
-        </Card.Body>
-      </Card>
+      <Container id="fl-detail-container">
+        <Card id="fl-detail-card">
+          <Card.Img id="fl-detail-img" variant="top" src={this.state.floatie.photo}/>
+          <Card.Body id="fl-detail-body">
+            <Card.Title id="fl-detail-title">{this.state.floatieName}</Card.Title>
+            <Card.Text>
+              {this.state.floatie.description}
+            </Card.Text>
+            <Card.Text>
+            {this.state.floatie.deliverable === true ?
+              <>
+                Can be delivered: <i className="fas fa-check"></i>
+              </>
+              :
+              <>
+                Can be delivered: <i className="fas fa-times"></i> 
+              </>
+            }
+            </Card.Text>     
+            <Card.Text>
+              {`Good for: ${this.state.floatie.goodFor}`}
+            </Card.Text>
+            <Card.Text>
+            {this.state.floatie.dogFriendly === true ?
+              <>
+                Dog Friendly: <i className="fas fa-paw"></i>
+              </>
+              :
+              <>
+                Dog Friendly: <i className="fas fa-times"></i> 
+              </>
+            }
+            </Card.Text>  
+            <Card.Text>
+              {`Price per day: $${this.state.floatie.price}`}
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </Container>
     );
   }
 }
